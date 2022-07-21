a12e1ee44acbece03fee5935ee4fd87edb5cd931
Angelica <jortega393@gmail.com>
2022-07-20 21:18:03 -0400

    prueba dos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class TrafficLight extends Component {
+  constructor() {
+    super();
+    this.state = {
+      estadoRoja: "apagada",
+      estadoAmarilla: "apagada",
+      estadoVerde: "apagada"
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <div id="barra"></div>
+        <div id="semaforo">
+          <div className="luz roja" id={this.state.estadoRoja}
+            onClick={() => this.setState({ estadoRoja: "encendida", estadoAmarilla: "apagada", estadoVerde: "apagada" })}>
+          </div>
+          <div className="luz amarilla" id={this.state.estadoAmarilla}
+            onClick={() => this.setState({ estadoRoja: "apagada", estadoAmarilla: "encendida", estadoVerde: "apagada" })}>
+          </div>
+          <div className="luz verde" id={this.state.estadoVerde}
+            onClick={() => this.setState({ estadoRoja: "apagada", estadoAmarilla: "apagada", estadoVerde: "encendida" })}>
+          </div>
+        </div>
+      </div>);
+  }
 }
 
-export default App;
+export default TrafficLight;
